a0b50da4c463d31fb3a7afdc33227bee5e66a6da
Jonathan Daniels <jd__93@hotmail.com>
2023-12-04 07:33:53 +0000

    Day 04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,10 +1,38 @@
+import kotlin.math.pow
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {line ->
+            val (winning, actual) = line.split(":")[1].trim().split("|").map { part ->
+                part.trim().split("\\s+".toRegex()).map {
+                    it.trim().toInt()
+                }
+            }
+            val size = winning.intersect(actual.toSet()).size
+            if (size == 0) {
+                0
+            } else {
+                2f.pow(size - 1).toInt()
+            }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val copies = List(input.size) { 1 }.toMutableList()
+        input.forEachIndexed { index, line ->
+            val (winning, actual) = line.split(":")[1].trim().split("|").map { part ->
+                part.trim().split("\\s+".toRegex()).map {
+                    it.trim().toInt()
+                }
+            }
+            val size = winning.intersect(actual.toSet()).size
+            val thisCopies = copies[index]
+            repeat(size) { idxAdj ->
+                val newIdx = index + idxAdj + 1
+                copies[newIdx] = copies[newIdx] + thisCopies
+            }
+        }
+        return copies.sum()
     }
 
     // test if implementation meets criteria from the description, like:
